b212dce1dd826dd58861b1be87da872dbf83d905
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2025-10-05 21:57:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
  
 
 const API_KEY = "AIzaSyDUSeK0ojUgFBuV98ddwRXwaKzNWJpv6Og"
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`
+// const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`
+const API_URL = `"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`
 
 const container = document.querySelector(".container")
 const chatsContainer = document.querySelector(".chats-container")
@@ -242,6 +243,7 @@ document.querySelectorAll(".suggestions-item").forEach(item =>{
 promptForm.addEventListener("submit", handleFormSubmit)
 
 promptForm.querySelector("#add-file-btn").addEventListener("click", () => fileInput.click())
+
 
 
 
